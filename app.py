ace9fbd775299674cecbd8403e89facc492faa28
darkex07 <itzchiku07@gmail.com>
2025-08-07 02:33:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -466,6 +466,7 @@ elif agent_mode == "Listener (Vent & Comfort)":
             st.markdown(f"<div style='background:#e0c3fc;padding:0.7em 1em;border-radius:0.8em;margin-bottom:0.2em;'><b>You:</b> {msg}</div>", unsafe_allow_html=True)
         else:
             st.markdown(f"<div style='background:#262730;color:#fff;padding:0.7em 1em;border-radius:0.8em;margin-bottom:1em;'><b>Agent:</b> {msg}</div>", unsafe_allow_html=True)
+            
 # STRESS RELIEVER SONG & DOWNLOAD SECTION
 st.markdown("""
 ---
